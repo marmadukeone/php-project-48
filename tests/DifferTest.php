4dd428f1ab28e85659b8a9bf53743443af1c90e9
Александр Сарапулов <marmadukeone@mail.com>
2023-05-02 17:46:49 +0700

    add new tests and fixtures

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -38,5 +38,31 @@ class DifferTest extends TestCase
         $resultTrue = file_get_contents($pathToTrueResult);
         $this->assertEquals($resultGendiff,$resultTrue);
     }
-
+    public function testGenDiffJsonUpgrate()
+    {
+        $pathToFixturesFile1 = __DIR__. "/fixtures/file1_2.json";
+        $pathToFixturesFile2 = __DIR__. "/fixtures/file2_2.json";
+        $pathToTrueResult = __DIR__. "/fixtures/result2";
+        $resultGendiff = genDiff($pathToFixturesFile1, $pathToFixturesFile2);
+        $resultTrue = file_get_contents($pathToTrueResult);
+        $this->assertEquals($resultGendiff,$resultTrue);
+    }
+    public function testGenDiffYmlUpgrate()
+    {
+        $pathToFixturesFile1 = __DIR__. "/fixtures/yml1_2.yml";
+        $pathToFixturesFile2 = __DIR__. "/fixtures/yml2_2.yml";
+        $pathToTrueResult = __DIR__. "/fixtures/result2";
+        $resultGendiff = genDiff($pathToFixturesFile1, $pathToFixturesFile2);
+        $resultTrue = file_get_contents($pathToTrueResult);
+        $this->assertEquals($resultGendiff,$resultTrue);
+    }
+    public function testGenDiffYamlUpgrate()
+    {
+        $pathToFixturesFile1 = __DIR__. "/fixtures/yaml1_2.yaml";
+        $pathToFixturesFile2 = __DIR__. "/fixtures/yaml2_2.yaml";
+        $pathToTrueResult = __DIR__. "/fixtures/result2";
+        $resultGendiff = genDiff($pathToFixturesFile1, $pathToFixturesFile2);
+        $resultTrue = file_get_contents($pathToTrueResult);
+        $this->assertEquals($resultGendiff,$resultTrue);
+    }
 }
