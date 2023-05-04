852ce67e7e8afe5cd41191f83430a975d1549266
Александр Сарапулов <marmadukeone@mail.com>
2023-05-04 18:42:00 +0700

    some fresh shit for my hater's vlad he like sosat' dicks

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -4,32 +4,76 @@ namespace Differ\Differ;
 
 use function Differ\Parsers\Parser\parseFile;
 
-function genDiff(string $pathFile1, string $pathFile2): string
+function genDiff(string $pathFile1, string $pathFile2)
 {
+
     $array1 = parseFile($pathFile1);
     $array2 = parseFile($pathFile2);
     //тут уже обработка по ассоциативному массиву
-    var_dump("START");
-    $result = transformToString($array1, $array2);
-    var_dump("END -- THIS RESULT:");
-    $result = $result[0];
-    var_dump($result);
+    $commonArray = transformToCommonArray($array1, $array2);
+    $result = transformToString($commonArray);
+    //$result = transformToString($array1, $array2);
+
+    // $result = $result[0];
+    var_dump("YAAAAAAA");
+    var_dump($commonArray);
     return $result;
 }
 
-function transformToString($arr, $depht = 1):string
+function transformToCommonArray(array $arr1, array $arr2): array
+{
+    $result = [];
+    foreach ($arr1 as $key => $value) {
+        if (array_key_exists($key, $arr2)) {
+            //ключ есть в обоих
+            if (is_array($value) && is_array($arr2[$key])) {
+                //вложенные
+                $temp = transformToCommonArray($value, $arr2[$key]);
+                var_dump("TEMP::", $temp);
+                if (!empty($temp)) {
+                    $result[$key]['value'] = $temp;
+                }
+            } else {
+                if ($value !== $arr2[$key]) {
+                    //значения не совпадают
+                    $result[$key]['value']['old']['value'] = $value;
+                    $result[$key]['value']['new']['value'] = $arr2[$key];
+                } else {
+                    //совпадают - простовалию
+                    $result[$key]['value']['value'] = $value;
+                }
+            }
+            //ну что как на это отреагирует влад?
+            unset($arr2[$key]);
+        } else {
+            //нету во втором
+            $result[$key]['value']['old']['value'] = $value;
+            $result[$key]['value']['new'] = null;
+        }
+    }
+    foreach ($arr2 as $key => $value) {
+        //нету в первом
+        $result[$key]['value']['old'] = null;
+        $result[$key]['value']['new']['value'] = $value;
+    }
+    return $result;
+}
+
+
+
+function transformToString($arr, $depht = 1): string
 {
     $nextDepht = $depht + 3;
     $spacesWithOperator = str_repeat(" ", $depht);
     // uncomment for debug depht:
     // $debugDephtStr = $depht % 10;
     // $spacesWithOperator = str_repeat($debugDephtStr, $depht);
-    
+
     $spacesWithoutOperator = $spacesWithOperator . str_repeat(" ", 2); // "+ " = 2 symbols
     $result = "{\n";
     foreach ($arr as $key => $value) {
-        if(array_key_exists('value', $value)) {
-            if(is_array($value['value'])) {
+        if (array_key_exists('value', $value)) {
+            if (is_array($value['value'])) {
                 $result .= "{$spacesWithoutOperator}{$key}: ";
                 $result .= transformToString($value['value'], $nextDepht);
                 $result .= $spacesWithoutOperator . "}\n";
@@ -48,12 +92,12 @@ function transformToString($arr, $depht = 1):string
                 $result .= $spacesWithOperator . formatRow($key, $value['new']['value'], "+");
             }
             continue;
-        } 
+        }
 
         if (is_null($value['new'])) {
             if (is_array($value['old']['value'])) {
                 $result .= "{$spacesWithOperator}- {$key}: ";
-                $result .= transformToString($value['old']['value'], $nextDepht);     
+                $result .= transformToString($value['old']['value'], $nextDepht);
                 $result .= $spacesWithoutOperator . "}\n";
             } else {
                 $result .= $spacesWithOperator . formatRow($key, $value['old']['value'], "-");
@@ -70,7 +114,7 @@ function transformToString($arr, $depht = 1):string
     if ($depht === 1) {
         $result .= "}\n";
     }
-    
+
     return $result;
 }
 
